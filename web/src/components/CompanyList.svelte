925a09b20cffe5f718033f1b19e680b4910308d8
Alicia Sykes <alicia@omg.lol>
2023-11-05 13:45:39 +0000

    Builds Astro site to fetch n render results

---
+++ web/src/components/CompanyList.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let companies = [];
+</script>
+
+{#if companies.length > 0}
+  <ul>
+    {#each companies as company}
+      <li>
+        <strong>{company.company}</strong>
+        <a href="{company.url}">Website</a>
+        <p>Contact: {company.contact}</p>
+        <p>Rewards: {company.rewards.join(', ')}</p>
+      </li>
+    {/each}
+  </ul>
+{:else}
+  <p>No companies to display.</p>
+{/if}
